df30a34fb886c5a3c31768d0a942b438ea8b8cae
fahrijal-alkuja <rizal.alkuja@gmal.com>
2022-11-01 22:37:08 +0800

    user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { IsNumber } from 'class-validator';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,7 +11,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-   constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
   create(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
     return this.prisma.user.create({
@@ -22,12 +27,12 @@ export class UsersService {
     return await this.prisma.user.findMany();
   }
 
-  async findOne(id: number ) {
+  async findOne(id: number) {
     const user = await this.prisma.user.findUnique({
       where: { id },
     });
     if (!user) {
-            throw new BadRequestException('User not found');
+      throw new BadRequestException('User not found');
     }
     return user;
   }
@@ -41,12 +46,12 @@ export class UsersService {
       throw new NotFoundException('User not found');
     } else {
       return await this.prisma.user.update({
-      where: { id },
-      data: {
-        name,
-        email,
-      },
-    });
+        where: { id },
+        data: {
+          name,
+          email,
+        },
+      });
     }
   }
 
@@ -54,12 +59,12 @@ export class UsersService {
     const user = await this.prisma.user.findUnique({
       where: { id },
     });
-    if (!user) { 
+    if (!user) {
       throw new NotFoundException('User not found');
     } else {
       return await this.prisma.user.delete({
-      where: { id },
-    });
+        where: { id },
+      });
     }
   }
 }
